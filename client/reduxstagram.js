71f42c5c42ee4bbe1ae70d6e3e826156200ee56c
amcereijo <amcereijo@gmail.com>
2016-12-01 20:56:40 +0100

    App Layout + Component Layout

---
+++ client/reduxstagram.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+// import css
+
+import css from './styles/style.styl';
+import Main from './components/main'
+
+render(<Main />, document.getElementById('root'));
