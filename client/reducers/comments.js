5fa8409b2f8e40eea1e729bdd9ccb33847f9a057
amcereijo <amcereijo@gmail.com>
2016-12-08 20:55:06 +0100

    Redux Reducer Composition

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,9 +1,34 @@
+function postComments(state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+    return [ ...state, {
+      user: action.author,
+      text: action.comment,
+    }]
+    case 'REMOVE_COMMENT':
+      console.log('removing comment!');
+      return [
+        ...state.slice(0, action.i),
+        ...state.slice(action.i + 1),
+      ];
+    default: return state;
+  }
+}
+
 // a reducer takes two things
 // 1. action (info about what happen)
 // 2. copy of current state
 
 function comments(state = [], action) {
   console.log(state, action);
+  if(typeof action.postId !== 'undefined') {
+    return {
+      // current state
+      ...state,
+      // reducer composition
+      [action.postId]: postComments(state[action.postId], action),
+    };
+  }
   return state;
 }
 export default comments;
